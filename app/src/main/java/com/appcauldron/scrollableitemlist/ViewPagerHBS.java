ee5ddebdf878c98962446685a66431c3b9775599
Hamdiovish <hamdy.b.s@orange.tn>
2015-07-21 16:29:59 +0100

    Custom "ViewPager"
    A custom "ViewPager" witch can calculate its height dynamically, instead the previous static height.

---
+++ app/src/main/java/com/appcauldron/scrollableitemlist/ViewPagerHBS.java
@@ -0,0 +1,46 @@
+package com.appcauldron.scrollableitemlist;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ViewPagerHBS  extends ViewPager {
+
+
+    /**
+     * Constructor
+     *
+     * @param context the context
+     */
+    public ViewPagerHBS(Context context) {
+        super(context);
+    }
+
+    /**
+     * Constructor
+     *
+     * @param context the context
+     * @param attrs the attribute set
+     */
+    public ViewPagerHBS(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = 0;
+        for(int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if(h > height) height = h;
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+}
